a651918ec31a9087b95db402b709b90b9f95f878
Mohan Sai Manthri <119850631+mohan-manthri@users.noreply.github.com>
2023-05-18 08:41:34 +0530

    Fixed on-screen keyboard blocking text fields (#82)
    - Resolves an issue where the keyboard was blocking the text fields.
    - Fixes the problem by removing the resizeToAvoidBottomInset
    - resizeToAvoidBottomInset is set to true by default
    
    Signed-off-by: Mohan Sai Manthri <119850631+mohan-manthri@users.noreply.github.com>

--- lib/route_preferences/route_preferences_screen.dart
+++ lib/route_preferences/route_preferences_screen.dart
@@ -69,7 +69,6 @@ class _RoutePreferencesScreenState extends State<RoutePreferencesScreen> with Ti
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.routePreferencesScreenTitle),
         bottom: PreferredSize(
